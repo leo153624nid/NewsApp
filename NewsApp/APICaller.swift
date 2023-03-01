25f6030841e111dfbfcd0bbd233de22cc46ff7a7
Алексей Чайкин <barjastroy@gmail.com>
2023-03-01 12:07:48 +0300

    Update APICaller.swift

--- NewsApp/APICaller.swift
+++ NewsApp/APICaller.swift
@@ -8,13 +8,28 @@
 import Foundation
 
 protocol APICallerProtocol {
-    var s : Int { get set }
+    var constants : ConstantsProtocol { get }
+}
+
+protocol ConstantsProtocol {
+    var apiKey: String { get }
+    var topHeadlinesURL: String { get }
+}
+
+final class Constants: ConstantsProtocol {
+    let apiKey : String
+    let topHeadlinesURL : String
+    
+    init(apiKey: String) {
+        self.apiKey = apiKey
+        self.topHeadlinesURL = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=\(apiKey)"
+    }
 }
 
 final class APICaller: APICallerProtocol {
     static var shared: APICallerProtocol = APICaller()
     
-    var s = 10
+    lazy var constants: ConstantsProtocol = Constants(apiKey: "b505953bf7614254a430c1a8bdea8e6a")
     
     private init() {}
 }
