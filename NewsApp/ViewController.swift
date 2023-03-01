a343c14a0adffae477bb0734c41940e68fb74569
Алексей Чайкин <barjastroy@gmail.com>
2023-03-01 11:47:47 +0300

    Update ViewController.swift

--- NewsApp/ViewController.swift
+++ NewsApp/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+// API key = b505953bf7614254a430c1a8bdea8e6a
+// url = https://newsapi.org/v2/everything?domains=wsj.com&apiKey=b505953bf7614254a430c1a8bdea8e6a
 
 class ViewController: UIViewController {
+    private var apiCaller: APICallerProtocol
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "News"
+        view.backgroundColor = .systemBackground
+        print(apiCaller.s)
     }
-
+    
+    init(with serviceManager: APICallerProtocol) {
+        self.apiCaller = serviceManager
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        self.apiCaller = APICaller.shared
+        super.init(coder: coder)
+    }
+    
 
 }
 
